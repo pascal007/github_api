dc01523ec07a0e56f66e7ddb11f1b7059048d16c
Pascal Ezeama <pascalezeama@gmail.com>
2023-01-29 22:17:48 +0100

    rest endpoint and other features

---
+++ main/app.py
@@ -0,0 +1,87 @@
+import os
+from flask import Flask, render_template, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+db_path = 'github_users.sqlite3'
+base_dir = os.path.abspath(os.path.dirname(__file__))
+db_uri = 'sqlite:///' + os.path.join(base_dir, 'database', 'github_users.sqlite')
+
+db = SQLAlchemy()
+migrate = Migrate()
+
+
+def create_app():
+    app = Flask(__name__)
+    os.makedirs('database', exist_ok=True)
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.app_context().push()
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    @app.route("/")
+    def index():
+        return render_template('index.html')
+
+    @app.route("/users/")
+    def users():
+        from models import GithubUsers
+        page = request.args.get('page')
+        per_page = request.args.get('pagination')
+        page = int(page) if (page and page.isdigit()) else 1
+        per_page = int(per_page) if (per_page and per_page.isdigit()) else 25
+        users_data = GithubUsers.query.order_by()
+        pages = users_data.paginate(page=page, per_page=per_page)
+
+        return render_template('users.html', users_data=users_data, pages=pages)
+
+    @app.route("/api/users/profiles", methods=['GET'])
+    def users_profile():
+        from models import GithubUsers
+        page = request.args.get('page')
+        per_page = request.args.get('pagination')
+        page = int(page) if (page and page.isdigit()) else 1
+        per_page = int(per_page) if (per_page and per_page.isdigit()) else 25
+        order_by = request.args.get('order_by')
+        username = request.args.get('username')
+        primary_key = request.args.get('id')
+
+        match order_by:
+            case 'id':
+                ordering = GithubUsers.id
+            case 'type':
+                ordering = GithubUsers.type
+            case _:
+                ordering = None
+
+        if not (username or primary_key):
+            results = GithubUsers.query.filter().order_by(ordering)
+        else:
+            results = None
+            if username:
+                results = GithubUsers.query.filter(username=username).order_by(ordering)
+            if primary_key and results:
+                results = results.filter(id=primary_key).order_by(ordering)
+            elif primary_key and not results:
+                results = GithubUsers.query.filter(id=primary_key).order_by(ordering)
+
+        results = results.paginate(page=page, per_page=per_page)
+
+        json_results = [result.as_dict() for result in results.items]
+
+        meta = {
+            "page": results.page,
+            "pages": results.pages,
+            "total_count": results.total,
+            "prev_page": results.prev_num,
+            "next_page": results.next_num,
+            "has_next": results.has_next,
+            "has_prev": results.has_prev
+        }
+
+        return jsonify({"data": json_results, "meta": meta})
+
+    return app
+
