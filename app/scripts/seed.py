8b91933e16e9cf193e9be7a50d3974992df8eac0
Pascal Ezeama <pascalezeama@gmail.com>
2023-01-29 12:24:57 +0100

    project restructure, switch to ORM

--- app/scripts/seed.py
+++ app/scripts/seed.py
@@ -1,30 +1,45 @@
 import requests
-import sqlite3
-from sqlite3 import OperationalError
+# from app.app import GithubUsers, db
+import sys
 
 API_URL = "https://api.github.com/users"
+seed_count = 150
 response = requests.get(url=API_URL).json()
 
 
-def get_db_connection():
-    conn = None
-    try:
-        conn = sqlite3.connect('../main/database/github_users.db')
-    except Exception as e:
-        print('Data not saved. Database connection error')
-        print(e)
-    return conn
+args = []
+for arg in range(1, len(sys.argv)):
+    args.append(sys.argv[arg])
+    if len(args) > 2:
+        break
 
+if args and len(args) > 1:
+    if args[0] in ['-t', '--total'] and args[1].isdigit():
+        SEED_COUNT = int(args[1])
 
-data = [
-    (data['id'], data['login'], data['avatar_url'], data['type'], data['url']) for data in response]
+data = []
+for x in range(0, seed_count):
+    print(x)
+    data.append({
+        'id': response[x]['id'],
+        'username': response[x]['login'],
+        'avatar': response[x]['avatar_url'],
+        'typ': response[x]['type'],
+        'url': response[x]['url'],
+    })
 
-conn = get_db_connection()
-if conn:
-    cursor = conn.cursor()
-    cursor.executemany("INSERT INTO users (id, username, avatar_url, type, url) values (%d,%s,%s,%s,%s)", data)
-    conn.commit()
-    print('Successfully saved')
+print(data)
+
+#
+#
+# data = [
+#     GithubUsers(id=data['id'], username=data['login'], avatar=data['avatar_url'],
+#                 typ=data['type'], url=data['url']) for data in response]
+#
+# db.session.add_all(data)
+# db.session.commit()
+# print('Successfully saved')
+
 
 
 
